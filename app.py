6927624e93bb39d3d5a762ce2bbd0a3abbb98e18
jamie cartmell <jamie@wordpressupdates.ie>
2023-10-06 14:10:26 +0100

    update wiht guni

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import (
-    Flask,
-    # jsonify,
-    render_template,
-    request,
-    redirect,
-    url_for,
-    session,
-    flash,
-)
+# from flask import (
+#     Flask,
+#     # jsonify,
+#     render_template,
+#     request,
+#     redirect,
+#     url_for,
+#     session,
+#     flash,
+# )
+from flask import Flask, redirect, render_template, url_for, session, flash, request
 
 # app = Flask(__name__ , static_url_path="/static")
 app = Flask(__name__)
